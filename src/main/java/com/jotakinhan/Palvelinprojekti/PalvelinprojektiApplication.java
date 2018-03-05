9b2222719d75815c5cd9429c6eb2324781fc9814
unknown <jotakinhan@gmail.com>
2018-03-05 22:45:47 +0200

    ensimmäinen commit

---
+++ src/main/java/com/jotakinhan/Palvelinprojekti/PalvelinprojektiApplication.java
@@ -0,0 +1,12 @@
+package com.jotakinhan.Palvelinprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalvelinprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalvelinprojektiApplication.class, args);
+	}
+}
